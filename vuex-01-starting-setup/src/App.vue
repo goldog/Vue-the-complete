17c27c7f99746028cd38b0c10f243f670c5b661d
goldog <goldog83@gmail.com>
2020-12-09 00:44:59 +0900

    212완

---
+++ vuex-01-starting-setup/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <favorite-value></favorite-value>
+    <button @click="addOne">add 10</button>
+    <change-count></change-count>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter  from './components/TheCounter.vue';
+import ChangeCount from './components/ChangeCount.vue';
+import FavoriteValue from './components/FavoriteValue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    ChangeCount,
+    FavoriteValue
+  },
+  methods: {
+    addOne () {
+      //this.$store.commit('increase', {value: 10});
+      this.$store.commit({
+        type: 'increase', 
+        value: 10
+      });
+    }
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
