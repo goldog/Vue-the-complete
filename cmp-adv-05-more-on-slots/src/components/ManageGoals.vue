d9952e1b652602a15aa87cddc271241d136d6760
goldog <goldog83@gmail.com>
2020-12-12 23:53:00 +0900

    116. Applying ...  완료

---
+++ cmp-adv-05-more-on-slots/src/components/ManageGoals.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <h2>Manage Goals</h2>
+    <input type="text" ref="goal"/>
+    <button @click="setGoal">Set goal</button>    
+    <error-alert v-if="inputIsInvalid">
+      <h2>입력이 이상해요</h2>
+      <p>값을 제대로 입력하세요</p>
+      <button @click="confirmError">확인</button>
+    </error-alert>    
+  </div>
+</template>
+
+<script>
+import ErrorAlert from './ErrorAlert.vue';
+
+export default {
+  components: { 
+    ErrorAlert, 
+
+  },
+  
+  data() {
+    return {
+      inputIsInvalid: false,
+    }
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === '') {
+        this.inputIsInvalid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsInvalid = false;
+    }
+  },
+}
+</script>
